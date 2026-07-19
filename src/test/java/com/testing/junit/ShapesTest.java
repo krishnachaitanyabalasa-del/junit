3b6c1c351da05dc31495df6887a61eb3f915e0db
Krishna Chaitanya Balasa <krishnachaitanyabalasa@gmail.com>
2026-07-19 10:48:58 +0530

    Assert True is used

--- src/test/java/com/testing/junit/ShapesTest.java
+++ src/test/java/com/testing/junit/ShapesTest.java
@@ -3,7 +3,7 @@ package com.testing.junit;
 import Calc.Shapes;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShapesTest {
     Shapes shape = new Shapes();
@@ -16,5 +16,17 @@ public class ShapesTest {
     @Test
     public void testCircleArea(){
         assertEquals(3.14,shape.circleArea(1),0.01,"Calculation is wrong");
+    }
+
+    @Test
+    public void testWrongSquareArea(){
+        assertNotEquals(575,shape.squareArea(24));
+    }
+
+    @Test
+    public void test_true(){
+        String s = "junit";
+        assertTrue(s.equals("junit"));
+
     }
 }
